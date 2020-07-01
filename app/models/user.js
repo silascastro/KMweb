35a0401cd471111b3bfc459ad5362f45c5d7b75b
silascastro <silascastro15@gmail.com>
2020-07-01 00:51:38 -0400

    adicionando sequelize e criando model de usuario

---
+++ app/models/user.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const User = sequelize.define('user', {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    });
+  
+    return User;
+}
